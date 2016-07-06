f9dc72cfd51ef124cc2788ac1e6742eb9eff4116
faysou <faysal.aberkane@gmail.com>
2016-07-06 23:09:49 +0100

    Updated release number

--- BootstrapInstall.m
+++ BootstrapInstall.m
@@ -5,7 +5,7 @@ Get["https://raw.githubusercontent.com/jkuczm/MathematicaBootstrapInstaller/v0.1
 
 BootstrapInstall[
 	"MTools",
-	"https://github.com/faysou/MTools/releases/download/0.1.1/MTools.zip",
+	"https://github.com/faysou/MTools/releases/download/0.1.2/MTools.zip",
 	"AdditionalFailureMessage" ->
 		Sequence[
 			"You can ",
